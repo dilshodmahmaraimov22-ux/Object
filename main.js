9d47fe5db197daf95f95358285e93a99cab9e670
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-07-01 01:35:27 +0500

    object qoshildi

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let person (){
-    ism = "Dilshod"
+let person = {
+    name : "Dilshod",
+    age : 18,
+    married : false,
 }
+
+let newperson = JSON.stringify(person);
+console.log(newperson);
